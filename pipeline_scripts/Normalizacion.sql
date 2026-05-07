d10225da212df018a6cb728d66c3ef99f13417f6
Andrés Isaac <zurcaled.serdna@gmail.com>
2026-05-06 19:48:40 -0600

    Update Normalizacion.sql

--- pipeline_scripts/Normalizacion.sql
+++ pipeline_scripts/Normalizacion.sql
@@ -288,8 +288,8 @@ SELECT
     t.id AS winning_team_id,
     t.full_name AS winning_team_name
 FROM raw.standings
-INNER JOIN Season s ON s.year = raw.attendance.year
-INNER JOIN Team t ON t.full_name = raw.attendance.team_name
+INNER JOIN Season s ON s.year = raw.standings.year
+INNER JOIN Team t ON t.full_name = raw.standings.team_name
 WHERE standings.sb_winner = 'Won Superbowl';
 
 --CREAR RESPALDOS
